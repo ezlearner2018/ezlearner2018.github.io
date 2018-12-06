6b4e9f3050d78f2c6e9631278931a8804237d0d9
EZLearn2018 <45574419+ezlearner2018@users.noreply.github.com>
2018-12-06 10:43:15 -0500

    Add files via upload

--- SelectResourse.js
+++ SelectResourse.js
@@ -7,26 +7,13 @@ $(function () {
         var y = document.getElementById("LearningStyles").selectedIndex;
         if (x == 0 || y == 0) {
             alert("Please fill all submission feilds.");
-        } else if (x == 1 && y == 1) {
-            //text for Physics & Visual
-            //a string to be displayed on the page.
-            $('#demo').html(``);
-        } else if (x == 1 && y == 2) {
-            //text for Physics & Audio
-            $('#demo').html(``);
-        } else if (x == 1 && y == 3) {
-            //text for Physics &
-            $('#demo').html(``);
-        } else if (x == 1 && y == 4) {
-            //text for Physics & K
-            $('#demo').html(``);
-        } else if (x == 2 && y == 1) {
+        }  else if (x == 1 && y == 1) {
             //text for Trig & Visual
             $('#demo').html(`
               <h2>
               <b><u>1. MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry
               <br><p2><b>URL:</b> <a href="https://www.mathalino.com/reviewer/plane-trigonometry">https://www.mathalino.com/reviewer/plane-trigonometry</a></p2>
               <br>
               </h2>
@@ -34,7 +21,7 @@ $(function () {
               <h2>
               <b><u>2. MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry, Youtube, Lectures
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry, Youtube, Lectures
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/channel/UCSoLIxNywmejhfPQefj-ZEw">https://www.youtube.com/channel/UCSoLIxNywmejhfPQefj-ZEw</a></p2>
               <br>
               </h2>
@@ -42,18 +29,18 @@ $(function () {
               <h2>
               <b><u>3. 3Blue1Brown</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry, Youtube, 3Blue1Brown,Computer Scinece, Applications
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry, Youtube, 3Blue1Brown,Computer Scinece, Applications
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw">https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw</a></p2>
               <br>
               </h2>
               `);
-        } else if (x == 2 && y == 2) {
+        } else if (x == 1 && y == 2) {
             //text for Trig & Auditory
             $('#demo').html(`
               <h2>
               <b><u>1. MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry
               <br><p2><b>URL:</b> <a href="https://www.mathalino.com/reviewer/plane-trigonometry">https://www.mathalino.com/reviewer/plane-trigonometry</a></p2>
               <br>
               </h2>
@@ -61,7 +48,7 @@ $(function () {
               <h2>
               <b><u>2. MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry, Youtube, Lectures
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry, Youtube, Lectures
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/channel/UCSoLIxNywmejhfPQefj-ZEw">https://www.youtube.com/channel/UCSoLIxNywmejhfPQefj-ZEw</a></p2>
               <br>
               </h2>
@@ -69,18 +56,18 @@ $(function () {
               <h2>
               <b><u>3. 3Blue1Brown</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Verbal, Calulus, Trigonometry, Youtube, 3Blue1Brown,Computer Scinece, Applications
+              <br><b>Tags:</b> Visual, Verbal, Calculus, Trigonometry, Youtube, 3Blue1Brown,Computer Scinece, Applications
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw">https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw</a></p2>
               <br>
               </h2>
               `);
-        } else if (x == 2 && y == 3) {
+        } else if (x == 1 && y == 3) {
           //text for Trig & Visual
             $('#demo').html(`
               <h2>
               <b><u>1. PHET TRIG TOUR</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Hands On, Calulus, Trigonometry, Application, Simulation, PHET
+              <br><b>Tags:</b> Hands On, Calculus, Trigonometry, Application, Simulation, PHET
               <br><p2><b>URL:</b> <a href="https://phet.colorado.edu/en/simulation/trig-tour">https://phet.colorado.edu/en/simulation/trig-tour</a></p2>
               <br>
               </h2>
@@ -88,17 +75,17 @@ $(function () {
               <h2>
               <b><u>2. eduMedia Tigronometric Circle</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Hands On, Calulus, Trigonometry, Application, Simulation, eduMedia
+              <br><b>Tags:</b> Hands On, Calculus, Trigonometry, Application, Simulation, eduMedia
               <br><p2><b>URL:</b> <a href="https://www.edumedia-sciences.com/en/media/284-trigonometric-circle">https://www.edumedia-sciences.com/en/media/284-trigonometric-circle</a></p2>
               <br>
               </h2>
             `);
-        } else if (x == 2 && y == 4) {
+        } else if (x == 1 && y == 4) {
             $('#demo').html(`
               <h2>
               <b><u>1. BetterExplained Trigonometry</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Reading/Writting, Calulus, Trigonometry, Application, Simulation, eduMedia
+              <br><b>Tags:</b> Reading/Writting, Calculus, Trigonometry, Application, Simulation, eduMedia
               <br><p2><b>URL:</b> <a href="https://betterexplained.com/articles/intuitive-trigonometry/">https://betterexplained.com/articles/intuitive-trigonometry/</a></p2>
               <br>
               </h2>
@@ -106,15 +93,15 @@ $(function () {
               <h2>
               <b><u>2. Khan Academy Trigonometry</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Reading/Writting, Calulus, Trigonometry, Application, Simulation, Khan Academy
+              <br><b>Tags:</b> Reading/Writting, Calculus, Trigonometry, Application, Simulation, Khan Academy
               <br><p2><b>URL:</b> <a href="https://www.khanacademy.org/math/geometry/hs-geo-trig/hs-geo-solve-for-an-angle/a/inverse-trig-functions-intro">https://www.khanacademy.org/math/geometry/hs-geo-trig/hs-geo-solve-for-an-angle/a/inverse-trig-functions-intro</a></p2>
               <br>
               </h2>`);
-        } else if (x == 3 && y == 1) {
+        } else if (x == 2 && y == 1) {
             $('#demo').html(`<h2>
               <b><u>1. Differential Calculus MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Reading, Calulus, Application, MATHalino
+              <br><b>Tags:</b> Visual, Reading, Calculus, Application, MATHalino
               <br><p2><b>URL:</b> <a href="https://www.mathalino.com/reviewer/differential-calculus/differential-calculus">https://www.mathalino.com/reviewer/differential-calculus/differential-calculus</a></p2>
               <br>
               </h2>
@@ -122,7 +109,7 @@ $(function () {
               <h2>
               <b><u>2. Derivation Introductions</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Auditory, Calulus, Application, khanacademy
+              <br><b>Tags:</b> Visual, Auditory, Calculus, Application, khanacademy
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/watch?v=EKvHQc3QEow&list=PL19E79A0638C8D449">https://www.youtube.com/watch?v=EKvHQc3QEow&list=PL19E79A0638C8D449</a></p2>
               <br>
               </h2>
@@ -130,36 +117,36 @@ $(function () {
               <h2>
               <b><u>3. PHET Calculus Grapher</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Hands On, Calulus, Application, Simulation, PHET
+              <br><b>Tags:</b> Visual, Hands On, Calculus, Application, Simulation, PHET
               <br><p2><b>URL:</b> <a href="https://phet.colorado.edu/en/simulation/calculus-grapher">https://phet.colorado.edu/en/simulation/calculus-grapher</a></p2>
               <br>
               </h2>
             `);
-        } else if (x == 3 && y == 2) {
+        } else if (x == 2 && y == 2) {
             $('#demo').html(`<h2>
               <b><u>1. Calulus Playlist</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Auditory, Calulus, Application, Youtube
+              <br><b>Tags:</b> Visual, Auditory, Calculus, Application, Youtube
               <br><p2><b>URL:</b> <a href="https://www.youtube.com/watch?v=EKvHQc3QEow&list=PL19E79A0638C8D449">https://www.youtube.com/watch?v=EKvHQc3QEow&list=PL19E79A0638C8D449</a></p2>
               <br>
               </h2>
             `);
-        } else if (x == 3 && y == 3) {
+        } else if (x == 2 && y == 3) {
             $('#demo').html(`
               <h2>
               <b><u>1. PHET Calculus Grapher</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Hands On, Calulus, Application, Simulation, PHET
+              <br><b>Tags:</b> Visual, Hands On, Calculus, Application, Simulation, PHET
               <br><p2><b>URL:</b> <a href="https://phet.colorado.edu/en/simulation/calculus-grapher">https://phet.colorado.edu/en/simulation/calculus-grapher</a></p2>
               <br>
               </h2>
             `);
-        } else if (x == 3 && y == 4) {
+        } else if (x == 2 && y == 4) {
             $('#demo').html(`
               <h2>
               <b><u>1. Differential Calculus MATHalino</u></b>
               <br><b>Subject:</b>  Mathmatics
-              <br><b>Tags:</b> Visual, Reading, Calulus, Application, MATHalino
+              <br><b>Tags:</b> Visual, Reading, Calculus, Application, MATHalino
               <br><p2><b>URL:</b> <a href="https://www.mathalino.com/reviewer/differential-calculus/differential-calculus">https://www.mathalino.com/reviewer/differential-calculus/differential-calculus</a></p2>
               <br>
               </h2>
