8e557cf80162e01ef34e751ecfec8e1578cd88ac
EZLearn2018 <45574419+ezlearner2018@users.noreply.github.com>
2019-01-22 22:38:21 -0500

    Update config.js

--- Node/config.js
+++ Node/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'wt5zVRdyVKXNazFhbmpcEJu5c',
-  consumer_secret:      'kiZtyhkpnpyYVAT7Hq5svyyPzcgGrVNddi8Vzg7n6fLZrYmy2s',
-  access_token:         '1052013635881570307-7Yh71J43RR19ynq13h3JE9HuUoUDjg',
-  access_token_secret:  'vdCdZI20RwrueqDZxCJc4gz92eMTLEMh6hPqCDggdp0Vr',
+  consumer_key:         'insert consumer_key here',
+  consumer_secret:      'insert consumer_secret here',
+  access_token:         'insert access_token here',
+  access_token_secret:  'insert access_token_secret here',
 }
